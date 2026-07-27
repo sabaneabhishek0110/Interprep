42d41d0ae5f81d7d732f83d0b3f01112ab05aa1d
Abhishek <sabaneabhishek0110@gmail.com>
2026-07-27 19:21:07 +0530

    Changed ai model of gemini gemini-flash-latest

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -18,7 +18,7 @@ export async function createFeedback(params: CreateFeedbackParams) {
       .join("");
 
     const { object } = await generateObject({
-      model: google("gemini-2.5-flash-001", {
+      model: google("gemini-flash-latest", {
         structuredOutputs: false,
       }),
       schema: feedbackSchema,
